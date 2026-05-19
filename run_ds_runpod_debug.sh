655059aada73fb7b3b2c30722dcdb896983227ee
Noor <k247625@nu.edu.pk>
2026-05-19 19:29:24 +0500

    fixing ds debug script path

--- run_ds_runpod_debug.sh
+++ run_ds_runpod_debug.sh
@@ -15,7 +15,7 @@ DS_XRAY="${DATA_ROOT}/ds_xray"
 DS_PATH_CLS="${DATA_ROOT}/ds_pathology_cls"
 DS_PATH_SEG="${DATA_ROOT}/ds_pathology_seg"
 
-PRETRAINED_CKPT=/workspace/output_dir/MedCoSS_Report_Xray_Path_RUNPOD_2GPU_DEBUG_2D_Path_1/checkpoint-0.pth
+PRETRAINED_CKPT=/tmp/output_dir/MedCoSS_Report_Xray_Path_RUNPOD_2GPU_DEBUG_2D_Path_1/checkpoint-0.pth
 
 gpu_id=0
 
