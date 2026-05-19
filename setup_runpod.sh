f59937f1a40db250a60a75a5367389d57d3e9c89
Noor <k247625@nu.edu.pk>
2026-05-19 19:38:40 +0500

    setup script updated

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -5,7 +5,7 @@ echo "== Updating apt =="
 apt update
 
 echo "== Installing system packages =="
-apt install -y libgl1 libglib2.0-0 git curl
+apt install -y libgl1 libglib2.0-0 git curl openssh-server python3-pip rsync tree
 
 echo "== Installing Python requirements =="
 pip install -r requirements_runpod.txt
